e8c4ba3168c3a76196b6a90f48b809b448783917
axelio-dev <axel.barth@yahoo.com>
2026-01-19 22:34:13 +0100

    Add /auth routes (login, register, logout)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import Link from "next/link";
+
+export default function HomePage() {
   return (
     <div>
-      <h1>Project successfully reprogrammed</h1>
+      <h1>Home page</h1>
+      <p>
+        To log in, click on{" "}
+        <Link href="/auth/login" className="text-blue-500 underline">
+          the login page
+        </Link>
+        .
+      </p>
+      <p>
+        To register, click on{" "}
+        <Link href="/auth/register" className="text-blue-500 underline">
+          the register page
+        </Link>
+        .
+      </p>
     </div>
   );
 }
