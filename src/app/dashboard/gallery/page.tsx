141f9ab8920c0c2563d908e08dc35ede09a5f852
axelio-dev <axel.barth@yahoo.com>
2026-02-20 19:05:08 +0100

    Creation of the different dashboard pages, integration of the navbar and sidebar

---
+++ src/app/dashboard/gallery/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { Rocket } from "lucide-react";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+
+export default function Gallery() {
+  const router = useRouter();
+  const { data: session, isPending } = authClient.useSession();
+
+  const handleLogout = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          toast.success("Safe flight, astronaut!");
+          router.push("/auth/login");
+          router.refresh();
+        },
+      },
+    });
+  };
+
+  if (isPending)
+    return (
+      <div className="min-h-screen bg-[#fff] flex items-center justify-center">
+        <Rocket className="text-red-500 animate-bounce w-10 h-10" />
+      </div>
+    );
+
+  return (
+    <main className="h-full w-full">
+      <Navbar />
+      <Sidebar />
+    </main>
+  );
+}
