f93900084300b469093a53826067153dd0834142
axelio-dev <axel.barth@yahoo.com>
2026-02-21 00:17:33 +0100

    Adding a title and subtitle to the different dashboard pages

--- src/app/dashboard/gallery/page.tsx
+++ src/app/dashboard/gallery/page.tsx
@@ -1,39 +1,46 @@
 "use client";
 
 import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
-import { toast } from "sonner";
-import { Rocket } from "lucide-react";
+import { Rocket, Camera } from "lucide-react";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import { exo } from "../../fonts";
 
 export default function Gallery() {
-  const router = useRouter();
   const { data: session, isPending } = authClient.useSession();
-
-  const handleLogout = async () => {
-    await authClient.signOut({
-      fetchOptions: {
-        onSuccess: () => {
-          toast.success("Safe flight, astronaut!");
-          router.push("/auth/login");
-          router.refresh();
-        },
-      },
-    });
-  };
 
   if (isPending)
     return (
-      <div className="min-h-screen bg-[#fff] flex items-center justify-center">
+      <div className="min-h-screen bg-white flex items-center justify-center">
         <Rocket className="text-red-500 animate-bounce w-10 h-10" />
       </div>
     );
 
   return (
-    <main className="h-full w-full">
+    <div className="flex flex-col h-screen">
       <Navbar />
-      <Sidebar />
-    </main>
+
+      <div className="flex flex-1">
+        <div className="block">
+          <Sidebar />
+        </div>
+        <main className="flex-1 overflow-y-auto p-8 bg-gray-50/30">
+          <div className="max-w-7xl mx-auto">
+            <div className="flex justify-between items-start mb-8">
+              <div>
+                <h1
+                  className={`${exo.className} text-4xl font-semibold text-gray-900`}
+                >
+                  Gallery
+                </h1>
+                <p className="text-gray-500 mt-3 text-md">
+                  Your portfolio of astronomical images
+                </p>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
   );
 }
